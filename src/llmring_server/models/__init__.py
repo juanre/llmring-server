a517224542c9aac880b58bb01a47415e652f8db6
Juan Reyero <juan@juanreyero.com>
2025-08-26 09:13:40 +0200

    Fixed imports

--- src/llmring_server/models/__init__.py
+++ src/llmring_server/models/__init__.py
@@ -1,1 +1,51 @@
+"""Models for llmring-server."""
 
+from llmring_server.models.conversations import (
+    Conversation,
+    ConversationBase,
+    ConversationCreate,
+    ConversationUpdate,
+    ConversationWithMessages,
+    Message,
+    MessageBase,
+    MessageBatch,
+    MessageCreate,
+)
+from llmring_server.models.receipts import Receipt, ReceiptRequest, ReceiptResponse
+from llmring_server.models.registry import LLMModel, ProviderInfo, RegistryResponse
+from llmring_server.models.usage import (
+    UsageLogRequest,
+    UsageLogResponse,
+    UsageSummary,
+    DailyUsage,
+    ModelUsage,
+    UsageStats,
+)
+
+__all__ = [
+    # Conversations
+    "Conversation",
+    "ConversationBase",
+    "ConversationCreate",
+    "ConversationUpdate",
+    "ConversationWithMessages",
+    "Message",
+    "MessageBase",
+    "MessageBatch",
+    "MessageCreate",
+    # Receipts
+    "Receipt",
+    "ReceiptRequest",
+    "ReceiptResponse",
+    # Registry
+    "LLMModel",
+    "ProviderInfo",
+    "RegistryResponse",
+    # Usage
+    "UsageLogRequest",
+    "UsageLogResponse",
+    "UsageSummary",
+    "DailyUsage",
+    "ModelUsage",
+    "UsageStats",
+]
